bdb4fdbddf6c1dd07f99f368f1942cf2619542a6
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2017-06-26 22:01:53 +0200

    remove build stuff

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.25-third-debug', 'build/scratch/ns3.25-mipv6-test1-debug', 'build/scratch/ns3.25-test-radvd-debug', 'build/scratch/ns3.25-mipv6-test2-debug', 'build/scratch/ns3.25-scratch-simulator-debug', 'build/scratch/ns3.25-mipv6-test4-debug', 'build/scratch/ns3.25-dup-debug', 'build/scratch/subdir/ns3.25-subdir-debug', 'build/scratch/ns3.25-second-debug', 'build/scratch/ns3.25-mipv6-test3-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
