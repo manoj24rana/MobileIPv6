37840fd1f440754f78fa5a79d5776e119a2f5e39
Manoj Kumar Rana <manoj24.rana@gmail.com>
2017-08-14 16:23:49 +0530

    Lena samples added in scratch

--- scratch/lena-ipv6-2.cc
+++ scratch/lena-ipv6-2.cc
@@ -35,10 +35,11 @@ using namespace ns3;
 
 /**
  * Sample simulation script for LTE+EPC. It instantiates several eNodeB,
- * attaches one UE per eNodeB starts a flow for each UE to  and from a remote host.
+ * attaches one UE per eNodeB starts a flow for remote host to  and from the first UE.
+ * It also  starts yet another flow between UE pair.
  */
 
-NS_LOG_COMPONENT_DEFINE ("EpcFirstExampleForIpv6");
+NS_LOG_COMPONENT_DEFINE ("EpcSecondExampleForIpv6");
 
 int
 main (int argc, char *argv[])
@@ -50,10 +51,10 @@ main (int argc, char *argv[])
 
   // Command line arguments
   CommandLine cmd;
-  cmd.AddValue ("numberOfNodes", "Number of eNodeBs + UE pairs", numberOfNodes);
-  cmd.AddValue ("simTime", "Total duration of the simulation [s])", simTime);
-  cmd.AddValue ("distance", "Distance between eNBs [m]", distance);
-  cmd.Parse (argc, argv);
+  cmd.AddValue("numberOfNodes", "Number of eNodeBs + UE pairs", numberOfNodes);
+  cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
+  cmd.AddValue("distance", "Distance between eNBs [m]", distance);
+  cmd.Parse(argc, argv);
 
   Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
   Ptr<PointToPointEpcHelper>  epcHelper = CreateObject<PointToPointEpcHelper> ();
@@ -81,6 +82,7 @@ main (int argc, char *argv[])
   p2ph.SetChannelAttribute ("Delay", TimeValue (Seconds (0.010)));
   NetDeviceContainer internetDevices = p2ph.Install (pgw, remoteHost);
 
+
   NodeContainer ueNodes;
   NodeContainer enbNodes;
   enbNodes.Create (numberOfNodes);
@@ -90,7 +92,7 @@ main (int argc, char *argv[])
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
   for (uint16_t i = 0; i < numberOfNodes; i++)
     {
-      positionAlloc->Add (Vector (distance * i, 0, 0));
+      positionAlloc->Add (Vector(distance * i, 0, 0));
     }
   MobilityHelper mobility;
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
@@ -123,7 +125,6 @@ main (int argc, char *argv[])
       ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress6 (), 1);
     }
 
-
   // Attach one UE per eNodeB
   for (uint16_t i = 0; i < numberOfNodes; i++)
     {
@@ -142,23 +143,19 @@ main (int argc, char *argv[])
   remoteHostStaticRouting->AddNetworkRouteTo (Ipv6Address ("7777:db80::"), Ipv6Prefix (56), internetIpIfaces.GetAddress (0, 1), 1, 0);
 
 
-  // interface 0 is localhost, 1 is the p2p device
-  Ipv6Address remoteHostAddr = internetIpIfaces.GetAddress (1, 1);
-
-
   // Install and start applications on UEs and remote host
 
   UdpEchoServerHelper echoServer (9);
 
-  ApplicationContainer serverApps = echoServer.Install (remoteHost);
+  ApplicationContainer serverApps = echoServer.Install (ueNodes.Get (0));
 
 
   serverApps.Start (Seconds (4.0));
   serverApps.Stop (Seconds (50.0));
 
 
-  UdpEchoClientHelper echoClient1 (remoteHostAddr, 9);
-  UdpEchoClientHelper echoClient2 (remoteHostAddr, 9);
+  UdpEchoClientHelper echoClient1 (ueIpIface.GetAddress (0,1), 9);
+  UdpEchoClientHelper echoClient2 (ueIpIface.GetAddress (0,1), 9);
 
   echoClient1.SetAttribute ("MaxPackets", UintegerValue (1000));
   echoClient1.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
@@ -168,8 +165,8 @@ main (int argc, char *argv[])
   echoClient2.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient2.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps1 = echoClient1.Install (ueNodes.Get(0));
-  ApplicationContainer clientApps2 = echoClient2.Install (ueNodes.Get(1));
+  ApplicationContainer clientApps1 = echoClient1.Install (remoteHost);
+  ApplicationContainer clientApps2 = echoClient2.Install (ueNodes.Get (1));
 
 
   clientApps1.Start (Seconds (4.0));
@@ -199,7 +196,7 @@ main (int argc, char *argv[])
 
   Ipv6StaticRoutingHelper routingHelper;
   Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> (&std::cout);
-  routingHelper.PrintRoutingTableAt (Seconds (4.3), pgw, routingStream);
+  routingHelper.PrintRoutingTableAt (Seconds (4.3), ueNodes.Get (1), routingStream);
 
 
 
